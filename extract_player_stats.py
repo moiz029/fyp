bcd1238a880d3860af4066d2968ab3ccd0c5251c
unknown <mehmoodhassan595@yahoo.com>
2022-03-19 11:49:34 +0500

    Different routes declared

---
+++ extract_player_stats.py
@@ -0,0 +1,6 @@
+def player_id(id):
+    return {
+        "playername":"Stats",
+        "playerid": id,
+        "working": True
+    }
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from glob import escape
+from flask import Flask,jsonify
+import extract_player_stats
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/allplayers")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return {'players':"Players stats"}
+
+@app.route("/getPlayer/<string:playerid>")
+def checking(playerid):
+    return jsonify(extract_player_stats.player_id(playerid))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
